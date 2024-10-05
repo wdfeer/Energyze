a33ce64451eed8cdf035a5009b91be382f359158
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-05 16:47:06 +0200

    Let duos shoot EnergizedCopper

--- src/energyze/items/EnergizedCopper.kt
+++ src/energyze/items/EnergizedCopper.kt
@@ -1,10 +1,28 @@
 package energyze.items
 
 import arc.graphics.Color
+import mindustry.content.Blocks
+import mindustry.entities.bullet.BasicBulletType
 import mindustry.type.Item
+import mindustry.world.blocks.defense.turrets.ItemTurret
 
 class EnergizedCopper : Item("energized-copper", Color.valueOf("df9d73")) {
     init {
         cost = 1.1f
+
+        (Blocks.duo as ItemTurret).ammoTypes.put(this, object : BasicBulletType(3f, 18f) {
+            init {
+                width = 7f
+                height = 9f
+                lifetime = 80f
+                ammoMultiplier = 2f
+
+                reloadMultiplier = 2f
+
+                lightningDamage = 9f
+                lightning = 2
+                lightningLength = 10
+            }
+        })
     }
 }
