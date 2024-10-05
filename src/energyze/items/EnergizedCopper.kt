42e1210337b9179b00895e73e63d98227da3479a
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-05 17:35:01 +0200

    Rebalance EnergizedCopper as ammo

--- src/energyze/items/EnergizedCopper.kt
+++ src/energyze/items/EnergizedCopper.kt
@@ -10,18 +10,18 @@ class EnergizedCopper : Item("energized-copper", Color.valueOf("df9d73")) {
     init {
         cost = 1.1f
 
-        (Blocks.duo as ItemTurret).ammoTypes.put(this, object : BasicBulletType(3f, 18f) {
+        (Blocks.duo as ItemTurret).ammoTypes.put(this, object : BasicBulletType(2.75f, 24f) {
             init {
                 width = 7f
                 height = 9f
-                lifetime = 80f
+                lifetime = 60f
                 ammoMultiplier = 2f
 
-                reloadMultiplier = 2f
+                reloadMultiplier = 1.5f
 
-                lightningDamage = 9f
+                lightningDamage = 6f
                 lightning = 2
-                lightningLength = 10
+                lightningLength = 8
             }
         })
     }
