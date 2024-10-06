e543d7df766508cdf11c398795626467b94f5016
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-06 16:28:17 +0200

    Create Energized scorch and hail classes

---
+++ src/energyze/blocks/EnergizedHail.kt
@@ -0,0 +1,12 @@
+package energyze.blocks
+
+import mindustry.content.Blocks
+import mindustry.world.blocks.defense.turrets.ItemTurret
+
+class EnergizedHail : ItemTurret("energized-scorch") {
+    init {
+        val hail = Blocks.hail as ItemTurret
+
+        // TODO
+    }
+}
---
+++ src/energyze/blocks/EnergizedScorch.kt
@@ -0,0 +1,12 @@
+package energyze.blocks
+
+import mindustry.content.Blocks
+import mindustry.world.blocks.defense.turrets.ItemTurret
+
+class EnergizedScorch : ItemTurret("energized-scorch") {
+    init {
+        val scorch = Blocks.scorch as ItemTurret
+
+        // TODO
+    }
+}
