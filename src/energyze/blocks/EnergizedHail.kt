5b734d8793d7d5bb68c2c895080375b678fd2fe3
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-06 16:33:22 +0200

    Implement EnergizedHail

--- src/energyze/blocks/EnergizedHail.kt
+++ src/energyze/blocks/EnergizedHail.kt
@@ -1,12 +1,28 @@
 package energyze.blocks
 
+import energyze.ModItems
 import mindustry.content.Blocks
+import mindustry.content.Items
+import mindustry.gen.Sounds
+import mindustry.type.Category
+import mindustry.type.ItemStack
 import mindustry.world.blocks.defense.turrets.ItemTurret
 
 class EnergizedHail : ItemTurret("energized-scorch") {
     init {
         val hail = Blocks.hail as ItemTurret
 
-        // TODO
+        requirements(Category.turret, ItemStack.with(ModItems.energizedCopper, 40, Items.graphite, 17))
+        ammoTypes = hail.ammoTypes.copy()
+        targetAir = false
+        reload = 40f
+        recoil = 2f
+        range = 275f
+        inaccuracy = 1f
+        shootCone = 10f
+        health = 400
+        shootSound = Sounds.bang
+        coolant = consumeCoolant(0.2f)
+        limitRange(0f)
     }
 }
