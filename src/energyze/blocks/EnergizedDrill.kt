52ddb119d90758adde42fdfc77ef8c4a7b9242e2
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-06 17:19:07 +0200

    Implement EnergizedDrill class

--- src/energyze/blocks/EnergizedDrill.kt
+++ src/energyze/blocks/EnergizedDrill.kt
@@ -1,9 +1,30 @@
 package energyze.blocks
 
+import energyze.ModItems
+import mindustry.content.Blocks
+import mindustry.content.Fx
+import mindustry.content.Items
 import mindustry.world.blocks.production.Drill
 
 class EnergizedDrill : Drill("energized-drill") {
     init {
-        // TODO
+        val blastDrill = Blocks.blastDrill as Drill
+
+        requirements(blastDrill.category, blastDrill.requirements.map { it.copy() }.onEach {
+            if (it.item == Items.copper) it.item = ModItems.energizedCopper
+            else if (it.item == Items.thorium) it.item = Items.phaseFabric
+        }.toTypedArray())
+
+        drillTime = blastDrill.drillTime / 3.6f
+        size = 4
+        drawRim = true
+        tier = 5
+        updateEffect = Fx.pulverize
+        updateEffectChance = 0.02f
+        drillEffect = Fx.mineHuge
+        rotateSpeed = 6f
+        itemCapacity = 50
+
+        consumeItem(ModItems.energizedCopper)
     }
 }
