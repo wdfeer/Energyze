1d4a895bbe53b7a5f5b6fd69834ba203650e85f3
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-08 18:30:29 +0200

    Create CoalEnergizer class

--- src/energyze/ModBlocks.kt
+++ src/energyze/ModBlocks.kt
@@ -6,6 +6,7 @@ object ModBlocks {
     init {
         CopperEnergizer()
         CopperDischarger()
+        CoalEnergizer()
 
         EnergizedDuo()
         EnergizedScorch()
---
+++ src/energyze/blocks/CoalEnergizer.kt
@@ -0,0 +1,36 @@
+package energyze.blocks
+
+import arc.graphics.Color
+import energyze.ModItems
+import mindustry.content.Fx
+import mindustry.content.Items
+import mindustry.gen.Sounds
+import mindustry.type.Category
+import mindustry.type.ItemStack
+import mindustry.world.blocks.production.GenericCrafter
+import mindustry.world.draw.DrawDefault
+import mindustry.world.draw.DrawFlame
+import mindustry.world.draw.DrawMulti
+
+class CoalEnergizer : GenericCrafter("coal-energizer") {
+    init {
+        requirements(Category.crafting, ItemStack.with(Items.copper, 180, Items.graphite, 80, Items.silicon, 60))
+        craftEffect = Fx.smeltsmoke
+        outputItem = ItemStack(ModItems.energizedCoal, 1)
+        craftTime = CRAFT_TIME
+        size = 2
+        hasPower = true
+        hasLiquids = false
+        drawer = DrawMulti(DrawDefault(), DrawFlame(Color.valueOf("fcf387")))
+        ambientSound = Sounds.techloop
+        ambientSoundVolume = 0.02f
+
+        consumeItems(ItemStack(Items.coal, 2))
+        consumePower(POWER_PER_OUTPUT / CRAFT_TIME)
+    }
+
+    companion object {
+        const val POWER_PER_OUTPUT = 500f
+        const val CRAFT_TIME = 60f
+    }
+}
