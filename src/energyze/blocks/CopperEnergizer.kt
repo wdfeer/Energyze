a59cd53f2deaf81334b3c6e30eea8845632da316
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-11 18:43:51 +0200

    Abstract energizer class

--- src/energyze/blocks/CopperEnergizer.kt
+++ src/energyze/blocks/CopperEnergizer.kt
@@ -1,36 +1,11 @@
 package energyze.blocks
 
-import arc.graphics.Color
 import energyze.ModItems
-import mindustry.content.Fx
 import mindustry.content.Items
-import mindustry.gen.Sounds
-import mindustry.type.Category
 import mindustry.type.ItemStack
-import mindustry.world.blocks.production.GenericCrafter
-import mindustry.world.draw.DrawDefault
-import mindustry.world.draw.DrawFlame
-import mindustry.world.draw.DrawMulti
 
-class CopperEnergizer : GenericCrafter("copper-energizer") {
-    init {
-        requirements(Category.crafting, ItemStack.with(Items.titanium, 300, Items.plastanium, 120, Items.phaseFabric, 80))
-        craftEffect = Fx.smeltsmoke
-        outputItem = ItemStack(ModItems.energizedCopper, 1)
-        craftTime = CRAFT_TIME
-        size = 2
-        hasPower = true
-        hasLiquids = false
-        drawer = DrawMulti(DrawDefault(), DrawFlame(Color.valueOf("fcf387")))
-        ambientSound = Sounds.techloop
-        ambientSoundVolume = 0.02f
-
-        consumeItems(ItemStack(Items.copper, 2))
-        consumePower(POWER_PER_OUTPUT / CRAFT_TIME)
-    }
-
-    companion object {
-        const val POWER_PER_OUTPUT = 1000f
-        const val CRAFT_TIME = 120f
-    }
-}
+class CopperEnergizer : Energizer("copper-energizer", Items.copper, ModItems.energizedCopper, arrayOf(
+    ItemStack(Items.titanium, 300),
+    ItemStack(Items.plastanium, 120),
+    ItemStack(Items.phaseFabric, 80)
+))
---
+++ src/energyze/blocks/Energizer.kt
@@ -0,0 +1,40 @@
+package energyze.blocks
+
+import arc.graphics.Color
+import mindustry.content.Fx
+import mindustry.gen.Sounds
+import mindustry.type.Category
+import mindustry.type.Item
+import mindustry.type.ItemStack
+import mindustry.world.blocks.production.GenericCrafter
+import mindustry.world.consumers.ConsumeItems
+import mindustry.world.consumers.ConsumePower
+import mindustry.world.draw.DrawDefault
+import mindustry.world.draw.DrawFlame
+import mindustry.world.draw.DrawMulti
+
+abstract class Energizer(name: String, input: Item, output: Item, requirements: Array<ItemStack>) : GenericCrafter (name) {
+    init {
+        this.requirements(Category.production, requirements)
+        craftEffect = Fx.smeltsmoke
+        outputItem = ItemStack(output, 1)
+        craftTime = CRAFT_TIME
+        size = 2
+        hasPower = true
+        hasLiquids = false
+        drawer = DrawMulti(DrawDefault(), DrawFlame(Color.valueOf("fcf387")))
+        ambientSound = Sounds.techloop
+        ambientSoundVolume = 0.02f
+
+        consumeItems(ItemStack(input, 2))
+        consumePower(POWER_PER_OUTPUT / CRAFT_TIME)
+    }
+
+    final override fun consumeItems(vararg items: ItemStack?): ConsumeItems = super.consumeItems(*items)
+    final override fun consumePower(powerPerTick: Float): ConsumePower = super.consumePower(powerPerTick)
+
+    companion object {
+        const val POWER_PER_OUTPUT = 1000f
+        const val CRAFT_TIME = 120f
+    }
+}
