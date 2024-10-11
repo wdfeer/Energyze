16f8e240a9a96f78c64efa06299cc2da0d4ee3b4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-11 19:00:59 +0200

    Add e-thorium as salvo ammo

--- src/energyze/items/EnergizedThorium.kt
+++ src/energyze/items/EnergizedThorium.kt
@@ -1,6 +1,10 @@
 package energyze.items
 
 import arc.graphics.Color
+import mindustry.content.Blocks
+import mindustry.content.Fx
+import mindustry.entities.bullet.BasicBulletType
+import mindustry.world.blocks.defense.turrets.ItemTurret
 
 class EnergizedThorium : EnergizedItem("energized-thorium", Color.valueOf("f9a3c7")) {
     init {
@@ -11,6 +15,21 @@ class EnergizedThorium : EnergizedItem("energized-thorium", Color.valueOf("f9a3c
     }
 
     fun addAmmo() {
-        // TODO
+        (Blocks.salvo as ItemTurret).ammoTypes.put(this, BasicBulletType(4f, 23f, "bullet").apply {
+            width = 10f
+            height = 13f
+            shootEffect = Fx.shootBig
+            smokeEffect = Fx.shootBigSmoke
+            ammoMultiplier = 4f
+            lifetime = 60f
+
+            reloadMultiplier = 1.2f
+
+            lightning = 1
+            lightningDamage = 13f
+
+            splashDamage = 16f
+            splashDamageRadius = 12f
+        })
     }
 }
