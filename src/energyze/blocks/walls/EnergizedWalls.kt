47999feeccb5624fa9b984b4906a457053efb27c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-19 10:52:03 +0200

    Register e-thorium walls

--- src/energyze/blocks/walls/EnergizedWalls.kt
+++ src/energyze/blocks/walls/EnergizedWalls.kt
@@ -7,5 +7,8 @@ object EnergizedWalls {
     init {
         EnergizedWall("energized-copper-wall", ModItems.energizedCopper, Blocks.copperWall, 2)
         EnergizedWall("energized-copper-wall-large", ModItems.energizedCopper, Blocks.copperWall, 3)
+
+        EnergizedWall("energized-thorium-wall", ModItems.energizedThorium, Blocks.thoriumWall, 2)
+        EnergizedWall("energized-thorium-wall-large", ModItems.energizedThorium, Blocks.thoriumWall, 3)
     }
 }
